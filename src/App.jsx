0d1f1346c761fe053c1511f81bef167b9549dec7
Ryotarou Shiozawa <pekoemood@gmail.com>
2025-03-16 22:22:47 +0900

    CRUD機能の実装完了

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,9 @@ import { v4 as uuid } from "uuid";
 
 const App = () => {
 	const [todos, setTodos] = useState([]);
-	const [text, setText] = useState('');
+	const [text, setText] = useState("");
+	const [titleText, setTitleText] = useState("");
+	const [isActive, setIsActive] = useState(false);
 
 	useEffect(() => {
 		const fetchData = async () => {
@@ -22,34 +24,53 @@ const App = () => {
 			title: text,
 			completed: false,
 			createdAt: new Date().toISOString(),
-			updatedAt: new Date().toISOString()
-		}
+			updatedAt: new Date().toISOString(),
+		};
 
-
-		const response = fetch('http://localhost:4000/todos', {
-			method: 'POST',
+		fetch("http://localhost:4000/todos", {
+			method: "POST",
 			headers: {
-				'Content-Type': 'application/json',
+				"Content-Type": "application/json",
 			},
 			body: JSON.stringify(data),
 		});
 
-		setText('');
+		setText("");
 		setTodos((pre) => [...pre, data]);
 	};
 
 	const handleDelete = async (todoId) => {
 		fetch(`http://localhost:4000/todos/${todoId}`, {
-			method: 'DELETE',
+			method: "DELETE",
 			headers: {
-				'Content-Type': 'application/json',
+				"Content-Type": "application/json",
 			},
 		});
 
 		setTodos((pre) => pre.filter((todo) => todo.id !== todoId));
 	};
 
-	console.log(todos);
+	const handleEdit = async (text, todoId) => {
+		const data = {
+			title: text,
+			updatedAt: new Date().toISOString(),
+		};
+
+		fetch(`http://localhost:4000/todos/${todoId}`, {
+			method: "PATCH",
+			headers: {
+				"Content-Type": "application/json",
+			},
+			body: JSON.stringify(data),
+		});
+
+		setTodos((pre) =>
+			pre.map((todo) =>
+				todo.id === todoId ? { ...todo, title: text } : todo,
+			),
+		);
+	};
+
 	return (
 		<>
 			<div className="m-8 flex flex-col items-center space-y-4">
@@ -62,15 +83,47 @@ const App = () => {
 					>
 						タスクの追加
 					</button>
-					<input onChange={(e) => setText(e.target.value)} type="text" placeholder="ここにタスクを入力" />
+					<input
+						onChange={(e) => setText(e.target.value)}
+						type="text"
+						placeholder="ここにタスクを入力"
+					/>
 				</div>
 				<ul className="list bg-base-100 rounded-box shadow-md">
 					<li className="p-4 pb-2 text-2xl opacity-60 tracking-wide">
 						タスク一覧
 					</li>
 					{todos.map((todo) => (
-						<li className="list-row flex justify-between" key={todo.id}>
-							<div className="justify-self-start">{todo.title}</div>
+						<li className="list-row flex justify-end" key={todo.id}>
+							<div className="justify-self-start">
+								{isActive ? (
+									<input
+										type="text"
+										value={titleText}
+										onChange={(e) => setTitleText(e.target.value)}
+									/>
+								) : (
+									todo.title
+								)}
+							</div>
+							{isActive ? (
+								<button
+									onClick={() => {setIsActive(!isActive); handleEdit(titleText, todo.id)}}
+									type="button"
+									className="btn btn-info"
+								>
+									保存
+								</button>
+							) : (
+								<button
+									onClick={() => {setIsActive(!isActive); setTitleText(todo.title)}}
+									type="button"
+									className="btn btn-info"
+								>
+									編集
+								</button>
+							)}
+
 							<button
 								onClick={() => handleDelete(todo.id)}
 								type="button"
